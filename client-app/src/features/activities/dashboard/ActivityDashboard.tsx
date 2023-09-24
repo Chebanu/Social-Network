d0da6d74c770cc3447d5ee630ed4df800e0e8567
Alexei <0alexchebanu@gmail.com>
2023-09-24 11:32:27 +0300

    refactoring MobX

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,42 +1,22 @@
 import { Grid } from "semantic-ui-react";
-import { Activity } from "../../../app/models/Activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActvityDetails";
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 
-interface Props {
-  activities: Activity[];
-
-  createOrEdit: (activity: Activity) => void;
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
-
-export default observer(function ActivityDashboard({
-  activities,
-  createOrEdit,
-  deleteActivity,
-  submitting,
-}: Props) {
+export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
   const { selectedActivity, editMode } = activityStore;
 
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList
-          activities={activities}
-          deleteActivity={deleteActivity}
-          submitting={submitting}
-        />
+        <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
         {selectedActivity && !editMode && <ActivityDetails />}
-        {editMode && (
-          <ActivityForm submitting={submitting} createOrEdit={createOrEdit} />
-        )}
+        {editMode && <ActivityForm />}
       </Grid.Column>
     </Grid>
   );
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,20 +1,12 @@
 import React, { SyntheticEvent, useState } from "react";
-import { Activity } from "../../../app/models/Activity";
 import { Label, Button, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props {
-  activities: Activity[];
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
+export default observer(function ActivityList() {
+  const { activityStore } = useStore();
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
 
-export default function ActivityList({
-  activities,
-
-  deleteActivity,
-  submitting,
-}: Props) {
   const [target, setTarget] = useState("");
 
   function handleActivityDelete(
@@ -25,11 +17,10 @@ export default function ActivityList({
     deleteActivity(id);
   }
 
-  const { activityStore } = useStore();
   return (
     <Segment>
       <Item.Group divided>
-        {activities.map((activity) => (
+        {activitiesByDate.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as="a">{activity.title}</Item.Header>
@@ -49,7 +40,7 @@ export default function ActivityList({
                 />
                 <Button
                   name={activity.id}
-                  loading={submitting && target === activity.id}
+                  loading={loading && target === activity.id}
                   onClick={(e) => handleActivityDelete(e, activity.id)}
                   floated="right"
                   content="Delete"
@@ -63,4 +54,4 @@ export default function ActivityList({
       </Item.Group>
     </Segment>
   );
-}
+});
